de5029e510c9e7f1cc14aa28a9473ad0b6597abb
Bikarbonate <vsenneville@yahoo.fr>
2020-09-18 12:54:35 +0200

    Follow Camera 1.0 a fix

---
+++ ThePsychic/Assets/Scripts/PlayerScripts/PlayerTransformContainer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerTransformContainer : MonoBehaviour
+{
+    [SerializeField] TransformData _playerTransformContainer;
+
+    private Transform _playerTransform;
+
+    private void Update()
+    {
+        if (_playerTransformContainer != null)
+        {
+            _playerTransformContainer.value = _playerTransform;
+        }
+    }
+}
---
+++ ThePsychic/Assets/Scripts/PlayerScripts/PlayerTransformReceiver.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerTransformReceiver : MonoBehaviour
+{
+    [SerializeField] TransformData _playerTransformData;
+
+    private Transform _thisTransform;
+
+    private void Awake()
+    {
+        _thisTransform = transform;
+    }
+
+    private void Update()
+    {
+        _thisTransform = _playerTransformData.value;
+    }
+}
