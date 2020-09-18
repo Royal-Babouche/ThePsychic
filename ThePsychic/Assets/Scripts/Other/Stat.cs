f2a2329cf23923567affd7b9637744f80b607073
Royal Babouche <carville.pro@gmail.com>
2020-09-18 14:01:07 +0200

    Update Stat.cs

--- ThePsychic/Assets/Scripts/Other/Stat.cs
+++ ThePsychic/Assets/Scripts/Other/Stat.cs
@@ -1,18 +1,61 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Stat : MonoBehaviour
+[System.Serializable]
+public class Stat 
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _value, _valueMin, _valueMax, _coef;
+
+    public Stat(float valueMax)
     {
-        
+        _value = valueMax;
+        _valueMin = 0;
+        _valueMax = valueMax;
+        _coef = _value / _valueMax;
     }
 
-    // Update is called once per frame
-    void Update()
+    public Stat(float value, float valueMax)
     {
-        
+        _value = value;
+        _valueMin = 0;
+        _valueMax = valueMax;
+        _coef = _value / _valueMax;
+    }
+
+    public Stat(float value, float valueMin, float valueMax)
+    {
+        _value = value;
+        _valueMin = valueMin;
+        _valueMax = valueMax;
+        _coef = _value / _valueMax;
+    }
+
+    public void AddValue(float value)
+    {
+        float result = _value + value;
+
+        if(result > _valueMax)  
+        {
+            _value = _valueMax;
+        }
+        else
+        {
+            _value = result;
+        }
+        _coef = _value / _valueMax; 
+    }
+
+    public void LessValue(float value)
+    {
+        float result = _value - value;
+
+        if (result < _valueMin)
+        {
+            _value = _valueMin;
+        }
+        else
+        {
+            _value = result;
+        }
+        _coef = _value / _valueMax;
     }
 }
